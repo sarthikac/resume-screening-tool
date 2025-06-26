00158fe7fa88c7d4f814bc805cbe738abe4fe0e0
Sarthika Chimmula <134342864+sarthikac@users.noreply.github.com>
2025-06-26 12:57:03 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+## 💡 `app.py`
+```python
+import fitz
+import spacy
+import spacy
+import os
+import streamlit as st
+from sentence_transformers import SentenceTransformer, util
+
+nlp = spacy.load
