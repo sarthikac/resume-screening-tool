91f7e35035f1ae4b112ce96010c45852cdd7abf7
Sarthika Chimmula <134342864+sarthikac@users.noreply.github.com>
2025-06-26 13:16:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-## 💡 `app.py`
-```python
 import fitz
-import spacy
 import spacy
 import os
 import streamlit as st
+from sklearn.cluster import KMeans
 from sentence_transformers import SentenceTransformer, util
+from io import StringIO
+import tempfile
 
-nlp = spacy.load
+nlp = spacy.load("en_core_web_sm")
+model = SentenceTransformer("all-MiniLM-L6-v2")
+SUPPORTED_TYPES = ("pdf", "docx", "txt")
+
+
+
