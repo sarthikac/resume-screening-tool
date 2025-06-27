e70869cbcf66af2357d70206a221cb5389f27681
Sarthika Chimmula <134342864+sarthikac@users.noreply.github.com>
2025-06-26 21:43:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import fitz
+import fitz  # PyMuPDF
 import spacy
 import os
+import docx
 import streamlit as st
 from sklearn.cluster import KMeans
 from sentence_transformers import SentenceTransformer, util
@@ -10,6 +11,21 @@ import tempfile
 nlp = spacy.load("en_core_web_sm")
 model = SentenceTransformer("all-MiniLM-L6-v2")
 SUPPORTED_TYPES = ("pdf", "docx", "txt")
+
+def extract_text(file): 
+  file_type = file.name.split(".")[-1].lower()
+  if file_type == "pdf":
+    doc = fitz.open(stream=file.read(), filetype="pdf")
+    return " ".join([page.get_text() for page in doc])
+  elif file_type == "docx":
+      doc = docx.Document(file)
+      return " ".join([para.text for para in doc.paragraphs])
+  elif file_type == "txt":
+      return StringIO(file.getvalue().decode("utf-8")).read()
+  else:
+      return ""
+
+
 
 
 
